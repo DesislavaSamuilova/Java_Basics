9225b3b4e46a7057ea13a6852964b2cf3ceef54d
DesislavaSamuilova <102139737+DesislavaSamuilova@users.noreply.github.com>
2022-04-11 22:54:41 +0300

    Add files via upload

---
+++ NestedLoop_Exercise/src/SumPrimeNonPrime_03.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SumPrimeNonPrime_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sumPrime = 0;
+        int sumNonPrime = 0;
+        String command = scanner.nextLine();
+        while (!command.equals("stop")) {
+            int number = Integer.parseInt(command);
+            if (number < 0) {
+                System.out.println("Number is negative.");
+                command = scanner.nextLine();
+                continue;
+            }
+            int count = 0;
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                sumPrime += number;
+            } else if(count > 2) {
+                sumNonPrime += number;
+            }
+            command = scanner.nextLine();
+        }
+        System.out.printf("Sum of all prime numbers is: %d%n", sumPrime);
+        System.out.printf("Sum of all non prime numbers is: %d", sumNonPrime);
+    }
+}
